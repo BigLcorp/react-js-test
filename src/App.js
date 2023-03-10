66294b77849f36de1aaab031bfb84d11cb35d207
BigLcorp <127101806+BigLcorp@users.noreply.github.com>
2023-03-10 08:04:59 +0700

    changes3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 function App() {
   return (
+
+    <body>
     <div className="App">
       <h1> Hello world </h1>
       <b>BIG L</b>
+      <img src="public/logo192.png" alt="logo"></img>
     </div>
+    </body>
   );
 }
 
